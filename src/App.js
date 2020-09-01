041649f0611626c5a8c704d0c390984e2aba1124
myname <richard.i.pota@gmail.com>
2020-09-01 15:42:41 +0200

    basic todo app with react hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoComp from './components/TodoComp';
 
 function App() {
   return (
@@ -19,6 +20,11 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <div>
+        <TodoComp/>
+      </div>
+
     </div>
   );
 }
